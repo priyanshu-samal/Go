6c23d94c07472da8997b0d3254bd8ad3c024d239
priyanshu-samal <samalpriyanshu966@gmail.com>
2026-01-11 11:50:17 +0530

    s-2

--- student/cmd/student/main.go
+++ student/cmd/student/main.go
@@ -1,10 +1,35 @@
 package main
 
-import(
-	"fmt"
-)
+import (
+	"log"
+     "fmt"
+	"github.com/priyanshu-samal/student/internal/config"
 
-func main(){
-  fmt.Println("Hello, Student!")
-  
+	"net/http"
+)
+func main() {
+
+	cfg := config.MustLoad()
+
+	router:=http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Welcome"))
+	}) 
+
+
+	server:=http.Server{
+		Addr:cfg.Addr,
+		Handler: router,
+
+	}
+
+    fmt.Printf("Server is running %s", cfg.HTTPServer.Addr)
+
+	err:=server.ListenAndServe()
+	if err!=nil{
+		log.Fatal("Failed to start server")
+	}
+
+	
 }
