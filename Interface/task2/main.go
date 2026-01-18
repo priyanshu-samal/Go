e64fcc880f82576635907e35005414b01d6ec8a7
priyanshu-samal <samalpriyanshu966@gmail.com>
2026-01-18 10:13:29 +0530

    14

--- Interface/task2/main.go
+++ Interface/task2/main.go
@@ -14,6 +14,8 @@ func (e Email) Notify() string {
 	return "Email sent to " + e.address
 }
 
+
+//n ye keh rha hai ki mujhe nhi pta notifier kya jhai mujhe ye pta hai ki vo notify frr sakta hai kese vo bhi nhi pta
 func send(n Notifier) {
 	fmt.Println(n.Notify())
 }
