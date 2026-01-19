86c46349f68f0d0d05ac12fa494aa389c371de5c
priyanshu-samal <samalpriyanshu966@gmail.com>
2026-01-19 12:03:55 +0530

    25

--- concurrency/task2/main.go
+++ concurrency/task2/main.go
@@ -1,6 +1,22 @@
 package main
 
+import "fmt"
 
-func main(){
-	
+type Counter struct {
+	x int
+}
+
+
+func (c *Counter) Inc() {
+	c.x++
+}
+
+func main() {
+	v := Counter{10}
+
+	v.Inc()
+	v.Inc()
+	v.Inc()
+
+	fmt.Println(v.x) 
 }
