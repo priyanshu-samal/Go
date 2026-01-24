38c96f047b9f4e1e50439a539566c979da11067f
priyanshu-samal <samalpriyanshu966@gmail.com>
2026-01-24 13:00:49 +0530

    55

--- stack/main.go
+++ stack/main.go
@@ -1,43 +1,44 @@
 package main
 
-import "fmt"
 
-type Stack struct {
-	data []int
+type Stack struct{
+	data[]int
 	size int
-	top  int
+	top int
 }
 
-func NewStack(size int) *Stack {
-	return &Stack{
-		data: make([]int, size),
-		top:  -1,
-		size: size,
+
+func NewStack(size int) *Stack{
+	return &Stack {
+		data: make([]int,size),
+		top:-1,
+		size:size,
 	}
 }
 
-func (s *Stack) Push(x int) bool {
-	if s.top == s.size-1 {
+
+func(s *Stack)Push( x int)bool{
+	if s.top==s.size-1{
 		return false
 	}
 	s.top++
-	s.data[s.top] = x
+	s.data[s.top]=x
 	return true
 }
 
-func (s *Stack) Pop() (int, bool) {
-	if s.top == -1 {
-		return 0, false
 
+func( s *Stack) Pop()(int,bool){
+	if s.top==-1{
+		return 0,false
 	}
-	x := s.data[s.top]
+	x:=s.data[s.top]
 	s.top--
-	return x, true
+	return x,true
 }
 
-func main() {
-	v:=NewStack(10)
-	fmt.Println(v.Push(10))
-	fmt.Println(v.data)
 
+
+func main(){
+
+     s:=NewStack(10)
 }
