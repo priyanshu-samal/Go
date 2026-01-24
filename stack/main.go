d5cd500249d8b7aa3dc0c9d74a3803cb3d33a9f9
priyanshu-samal <samalpriyanshu966@gmail.com>
2026-01-24 12:51:01 +0530

    53

---
+++ stack/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Stack struct {
+	data []int
+	size int
+	top  int
+}
+
+func NewStack(size int) *Stack {
+	return &Stack{
+		data: make([]int, size),
+		top:  -1,
+		size: size,
+	}
+}
+
+func (s *Stack) Push(x int) bool {
+	if s.top == s.size-1 {
+		return false
+	}
+	s.top++
+	s.data[s.top] = x
+	return true
+}
+
+func (s *Stack) Pop() (int, bool) {
+	if s.top == -1 {
+		return 0, false
+
+	}
+	x := s.data[s.top]
+	s.top--
+	return x, true
+}
+
+func main() {
+	v:=NewStack(10)
+	fmt.Println(v.Push(10))
+	fmt.Println(v)
+
+}
