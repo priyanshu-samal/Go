7fd3c030648863ffd824b47118d59590326a81a6
priyanshu-samal <samalpriyanshu966@gmail.com>
2026-01-28 11:50:09 +0530

    59

--- net/main.go
+++ net/main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"log/slog"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	})
 
-    http.HandleFunc("/",handlefunc)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
-
-
-}
-
-func handlefunc(w http.ResponseWriter, r *http.Request){
-      wc,err:=w.Write([]byte("Hello world"))
-	  if err!=nil{
-		slog.Error("error writing resposnse",err)
-		return
-	  }
-	  fmt.Println("%d bytes writen",wc)
+	http.ListenAndServe(":8080", nil)
 }
